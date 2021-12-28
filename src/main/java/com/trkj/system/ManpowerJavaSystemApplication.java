387133ea62cd42c337f29ad1d3a231c7698764d4
kuangshen <3210053361@qq.com>
2021-12-28 16:15:19 +0800

    wx

--- src/main/java/com/trkj/system/ManpowerJavaSystemApplication.java
+++ src/main/java/com/trkj/system/ManpowerJavaSystemApplication.java
@@ -1,9 +1,10 @@
 package com.trkj.system;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
+@MapperScan("com.example.**.mapper")
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ManpowerJavaSystemApplication {
 
