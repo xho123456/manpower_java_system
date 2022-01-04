ae8c847cfa1fc0f78acf92e8dc5e0fb5c6e25cf1
@2944763470qq.com <2944763470@qq.com>
2022-01-04 10:09:40 +0800

    完善

---
+++ src/main/java/com/trkj/system/staff_management/entity/StaffHistoryEntity.java
@@ -0,0 +1,163 @@
+package com.trkj.system.staff_management.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)  /** 链式写法 */
+public class StaffHistoryEntity {
+    private Long staffId;
+    /** 员工编号 */
+
+    private String staffName;
+    /** 员工姓名 */
+
+//    private String staffSex;
+//    /** 员工性别 */
+
+    private Long staffPhone;
+    /** 员工手机号码 */
+
+    private String staffEmail;
+    /** 员工邮箱 */
+//
+//    private String staffPicture;
+//    /** 员工照片 */
+//
+//    private Date staffBirthday;
+//    /** 员工生日 */
+
+//    private String staffOutlook;
+//    /** 政治面貌 */
+
+//    private String staffEducation;
+//    /** 学历 */
+//
+//    private String DEPTPOSTID;
+//    /** 职位编号 */
+
+//    private String staffPass;
+//    /** 密码 */
+
+    private Date staffHIREDATE;
+    /** 入职日期 */
+
+//    private long workerId;
+//    /** 转正编号 */
+//
+//    private String staffIdentity;
+//    /** 员工身份证 */
+//
+//    private long deptId;
+//    /** 部门编号 */
+
+    private String deptName;
+    /** 部门名称 */
+
+//    private String staffMajor;
+//    /** 员工专业 */
+//
+//    private long staffEmergency;
+//    /** 紧急联系人 */
+//
+//    private String staffWechat;
+//    /** 微信 */
+//
+//    private String staffCredit;
+//    /** 银行卡号 */
+//
+//    private String staffBlood;
+//    /** 血型 */
+//
+//    private String staffSign;
+//    /** 星座 */
+//
+//    private String staffMarital;
+//    /** 婚姻状态 */
+//
+//    private String staffRegistered;
+//    /** 户口所在地 */
+//
+//    private String staffSchool;
+//    /** 毕业学校 */
+//
+//    private String staffAddress;
+//    /** 现住地址 */
+//
+//    private Long WORKEXPERIENCEID;
+//    /** 工作经历编号 */
+
+//    private Date WORKSTARETIME;
+//    /** 工作开始时间 */
+//
+    private Date FORMAL_QUIT_DATE;
+    /** 离职时间 */
+
+    private String QUIT_EXPLAIN;
+    /** 离职原因 */
+//
+//    private String COMPANYNAME;
+//    /** 公司名称 */
+
+    private String POSTNAME;
+    /** 职位名称 */
+
+    private Long gl;
+    /** 工龄 */
+//
+//    private Long EDUCATIONID;
+//    /** 教育经历编号 */
+//
+//    private Date EDUCATIONSTARTTIME;
+//    /** 教育开始时间 */
+//
+//    private Date EDUCATIONENDTIME;
+//    /** 教育结束时间 */
+
+//    private String EDUCATIONSTUDENTNAME;
+//    /** 学校名称 */
+//
+//    private Long GLORYID;
+//    /** 获奖编号 */
+//
+//    private String GLORYNAME;
+//    /** 获奖名称 */
+//
+//    private String GLORYUNITNAME;
+//    /** 颁发单位名称 */
+//
+//    private String GLORYREMARK;
+//    /** 备注 */
+//
+//    private Long PUNISHID;
+//    /** 惩罚编号 */
+//
+//    private String PUNISHCAUSE;
+//    /** 惩罚原因 */
+
+//    private String PUNISHTYPE;
+//    /** 惩罚类型 */
+//
+//    private String PUNISHUNIT;
+//    /** 惩罚单位 */
+//
+//    private Long ISREVOCATION;
+//    /** 是否撤销 */
+//
+//    private Date CREATEDTIME;
+//    /** 创建时间 */
+//
+//    private Date UPDATEDTIME;
+//    /** 修改时间 */
+//
+//    private Long ISDELETED;
+//    /** 逻辑删除 */
+
+
+}
---
+++ src/main/java/com/trkj/system/staff_management/mapper/Staffmapper.java
@@ -0,0 +1,107 @@
+package com.trkj.system.staff_management.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.trkj.system.staff_management.entity.*;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface Staffmapper extends BaseMapper<StaffEntity> {
+    /**
+     * 查看所有员工
+     * */
+    @Select(" SELECT s.STAFF_NAME,s.STAFF_ID,d.DEPT_NAME,dp.POST_NAME," +
+            "  s.STAFF_PHONE,s.STAFF_OUTLOOK,s.STAFF_HIREDATE" +
+            "  FROM  STAFF s  LEFT JOIN dept d on d.DEPT_ID=s.DEPT_ID LEFT JOIN DEPT_POST dp on dp.DEPT_ID=d.DEPT_ID"+
+            "  where s.IS_DELETED=0 " )
+    IPage<StaffEntity> findStaff(Page<StaffEntity> page);
+
+    /**
+     * 查看离职员工
+     */
+    @Select(" SELECT s.STAFF_NAME,s.STAFF_ID,d.DEPT_NAME,dp.POST_NAME,s.STAFF_PHONE," +
+            "s.staff_Email,s.STAFF_HIREDATE,q.FORMAL_QUIT_DATE,q.QUIT_EXPLAIN," +
+            "TRUNC((TO_CHAR(q.FORMAL_QUIT_DATE,'yyyy')-TO_CHAR(s.STAFF_HIREDATE,'yyyy'))) as gl " +
+            "FROM  QUIT q LEFT JOIN  STAFF s on q.STAFF_ID=s.STAFF_ID LEFT JOIN dept d on d.DEPT_ID=q.DEPT_ID " +
+            "LEFT JOIN DEPT_POST dp on dp.DEPT_POST_ID=s.DEPT_POST_ID  where s.IS_DELETED=0 " )
+    IPage<StaffHistoryEntity> findHistoryStaff(Page<StaffHistoryEntity> page);
+
+    /**
+     * 查看精英员工
+     */
+    @Select("SELECT s.STAFF_ID,s.STAFF_NAME,s.STAFF_HIREDATE,s.STAFF_PHONE,s.STAFF_EMAIL,s.STAFF_OUTLOOK,s.STAFF_EDUCATION," +
+            "s.STAFF_SCHOOL,d.DEPT_NAME,dp.POST_NAME FROM STAFF s LEFT JOIN DEPT d on s.DEPT_ID=d.DEPT_ID LEFT JOIN " +
+            "DEPT_POST dp on dp.DEPT_POST_ID=s.DEPT_POST_ID WHERE dp.POST_NAME='经理' ")
+    IPage<StaffEliteEntity> findEliteStaff(Page<StaffEliteEntity> page);
+
+    /**
+     * 查询所有获奖员工
+     */
+    @Select("<script> "+
+            "SELECT g.GLORY_NAME,g.GLORY_UNITNAME,g.GLORY_REMARK,g.CREATED_TIME,s.STAFF_ID,s.STAFF_NAME,d.DEPT_NAME,dp.POST_NAME" +
+            " FROM GLORY g,STAFF s,DEPT d,DEPT_POST dp WHERE " +
+            " g.STAFF_ID=s.STAFF_ID and d.DEPT_ID=s.DEPT_ID and dp.DEPT_POST_ID=s.DEPT_POST_ID " +
+            "</script>")
+    IPage<StaffRewardEntity> findRewardStaff(Page<StaffRewardEntity> page);
+
+    /**
+     * 根据ID查询获奖员工
+     * @param id
+     * @return
+     */
+//    @Select("<script> "+
+//            "SELECT g.GLORY_ID,g.GLORY_NAME,g.GLORY_UNITNAME,g.GLORY_REMARK,g.CREATED_TIME,s.STAFF_ID,s.STAFF_NAME FROM GLORY g,STAFF s WHERE\n" +
+//            " g.STAFF_ID=s.STAFF_ID and s.STAFF_ID = #{id} " +
+//            "</script>")
+//    List<StaffEntity> findCreatedById(Long id);
+
+    /**
+     * 根据名字查询获奖员工
+     * @param name
+     * @return
+     */
+//    @Select("<script> "+
+//            "SELECT g.GLORY_ID,g.GLORY_NAME,d.DEPT_ID,d.DEPT_NAME,s.STAFF_ID,s.STAFF_SEX,\n" +
+//            " s.STAFF_NAME,s.STAFF_PHONE,s.DEPT_ID,s.POSITION_NAME FROM GLORY g,dept d,STAFF s WHERE\n" +
+//            " g.STAFF_ID=s.STAFF_ID and d.DEPT_ID=s.DEPT_ID and s.STAFF_NAME like #{name} " +
+//            "</script>")
+//    List<StaffEntity> findCreatedLikeByName(String name);
+
+    /**
+     * 查看所有惩罚的员工
+     */
+    @Select("SELECT p.CREATED_TIME,p.PUNISH_CAUSE,p.PUNISH_TYPE,p.PUNISH_UNIT,d.DEPT_ID,d.DEPT_NAME,s.STAFF_ID," +
+            "s.STAFF_NAME,dp.POST_NAME FROM PUNISH p," +
+            "dept d,STAFF s,DEPT_POST dp WHERE p.STAFF_ID=s.STAFF_ID and d.DEPT_ID=s.DEPT_ID and dp.DEPT_POST_ID=s.DEPT_POST_ID ")
+    IPage<StaffPunishmentEntity> findPunishmentStaff(Page<StaffPunishmentEntity> page);
+
+    /**
+     * 根据ID查询所有惩罚的员工
+     * @param id
+     * @return
+     */
+//    @Select("<script>" +
+//            " SELECT p.PUNISH_ID,p.PUNISH_CAUSE,d.DEPT_ID,d.DEPT_NAME,s.STAFF_ID,s.STAFF_SEX," +
+//            "s.STAFF_NAME,s.STAFF_PHONE,s.POSITION_NAME FROM PUNISH p,dept d,STAFF s " +
+//            "WHERE p.STAFF_ID=s.STAFF_ID and d.DEPT_ID=s.DEPT_ID and s.STAFF_ID = #{id}" +
+//            "</script>")
+//    List<StaffEntity> findPunishById(Long id);
+
+    /**
+     * 根据名字查询所有惩罚的员工
+     * @param name
+     * @return
+     */
+//    @Select("<script>" +
+//            " SELECT p.PUNISH_ID,p.PUNISH_CAUSE,d.DEPT_ID,d.DEPT_NAME,s.STAFF_ID,s.STAFF_SEX," +
+//            "s.STAFF_NAME,s.STAFF_PHONE,s.POSITION_NAME FROM PUNISH p,dept d,STAFF s " +
+//            "WHERE p.STAFF_ID=s.STAFF_ID and d.DEPT_ID=s.DEPT_ID and s.STAFF_ID like #{name} " +
+//            "</script>")
+//    List<StaffEntity> findPunishLikeByName(String name);
+
+}
